e381ece260ea3218a156bf1f22056981c133c2b0
Maddie Williams <maddie.williams@tpximpact.com>
2023-01-30 15:52:14 +0000

    Display individual data service record

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class DataServicesController < ApplicationController
+  def show
+    @data_service = DataService.find(params["id"])
+  end
   def index
     @organisations = Organisation.includes(:data_services).all
   end
+end
+        
+
+
+    end
 end
