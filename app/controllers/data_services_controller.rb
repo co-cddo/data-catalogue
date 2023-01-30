3e9eca17aee9314004adc4327a5f293c4b9c4d6a
Maddie Williams <maddie.williams@tpximpact.com>
2023-01-30 15:53:26 +0000

    Remove unnecessary whitespace

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 class DataServicesController < ApplicationController
-  def show
-    @data_service = DataService.find(params["id"])
-  end
   def index
     @organisations = Organisation.includes(:data_services).all
   end
-end
-        
-
-
-    end
+  
+  def show
+    @data_service = DataService.find(params["id"])
+  end
 end
