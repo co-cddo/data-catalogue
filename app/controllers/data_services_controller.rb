60efa1444bb99c993d0b0e371e64ab138bf7cb02
Nicholas Wieland <ngw@nofeed.org>
2023-01-31 12:21:06 +0000

    Rubocop is now happy

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -4,8 +4,8 @@ class DataServicesController < ApplicationController
   def index
     @organisations = Organisation.includes(:data_services).all
   end
-  
+
   def show
-    @data_service = DataService.find(params["id"])
+    @data_service = DataService.find(params[:id])
   end
 end
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Organisation < ApplicationRecord
-  has_many :data_services
+  has_many :data_services, dependent: true
 end
