117e317694b3218094189fb0849e950b75bb44b2
Maddie Williams <maddie.williams@tpximpact.com>
2023-01-31 16:50:52 +0000

    Fix spec typo

--- spec/requests/data_services_spec.rb
+++ spec/requests/data_services_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "DataServices", type: :request do
-  describe "GET /data_services" do
+  describe "GET /data_service" do
     it "returns data_services" do
       get data_services_path
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe "GET /data_services" do
+  describe "GET /data_service" do
     let(:data_service) { create(:data_service) }
 
     it "returns data_services" do
