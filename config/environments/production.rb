74a7af2041af39e1c6e1a05066b5f89a3f00c275
Nicholas Wieland <ngw@nofeed.org>
2023-02-02 14:23:18 +0000

    Similar build to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,10 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = nil
-  config.sass.style = :compressed
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.compress = true
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
