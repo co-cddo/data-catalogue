e382fc203dc5bfb924b0ac0afeaa008a68307b52
Nicholas Wieland <ngw@nofeed.org>
2023-02-03 16:21:47 +0000

    HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with  name: ENV.fetch('HTTP_USERNAME'), 
+                                password: ENV.fetch('HTTP_PASSWORD')
 end
---
+++ spec/support/auth_helper.rb
@@ -0,0 +1,9 @@
+module AuthHelper
+  def http_auth_headers
+    username = ENV.fetch('HTTP_USERNAME')
+    password = ENV.fetch('HTTP_PASSWORD')
+    {
+      Authorization: ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
+    }
+  end
+end
