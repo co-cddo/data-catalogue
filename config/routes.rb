b907c349b236bdc8dd7a504c4e64c323afd30903
Nicholas Wieland <ngw@nofeed.org>
2023-02-13 16:37:31 +0000

    Enable authentication for Sidekiq::Web

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@
 require 'sidekiq/web'
   
 Rails.application.routes.draw do
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["HTTP_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["HTTP_PASSWORD"]))
+  end
+  
   mount Sidekiq::Web => '/sidekiq'
 
   resources :data_services, only: %i[index show]
