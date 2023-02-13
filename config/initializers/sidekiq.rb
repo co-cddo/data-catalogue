27967f2edad5a378fb62ddddcfa599dc367c4902
Nicholas Wieland <ngw@nofeed.org>
2023-02-13 15:39:13 +0000

    Amend redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,11 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
+    url: ENV.fetch("REDIS_URL")
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    host: ENV['REDIS_HOST'],
-    port: ENV['REDIS_PORT'] || '6379'
+    url: ENV.fetch("REDIS_URL")
   }
 end
