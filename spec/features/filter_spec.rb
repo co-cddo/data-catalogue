05e99bbf6c60fd1370ffcc0be6506fbc1a083235
Maddie Williams <maddie.williams@tpximpact.com>
2023-02-15 15:31:38 +0000

    Rubocop

--- spec/features/filter_spec.rb
+++ spec/features/filter_spec.rb
@@ -15,15 +15,15 @@ RSpec.describe 'Filters' do
     it 'User loads the page with all services but the clear filters button is not visible' do
       visit '/'
       expect(page).to have_selector('div.govuk-checkboxes--small div', count: 6)
-      expect(page).to_not have_selector('a.govuk-link.govuk-link--no-visited-state[data-cy="data-service-clear-filters-button"]')
+      expect(page).not_to have_selector('a.govuk-link.govuk-link--no-visited-state')
     end
 
-    it 'User loads page, filters by organisation, the results are displayed and clear filters button becomes visible' do
+    it 'User loads page, filters by organisation, the results are displayed, clear button becomes visible' do
       visit '/'
       check("filters_#{organisation_test.id}")
       click_button 'Apply filters'
       expect(page).to have_selector('ul.data-services li', count: 1)
-      expect(page).to have_selector('a.govuk-link.govuk-link--no-visited-state[data-cy="data-service-clear-filters-button"]', count: 1)
+      expect(page).to have_selector('a.govuk-link.govuk-link--no-visited-state')
     end
 
     it 'User loads page, filters by organisation and then removes the filters which then displays all services' do
