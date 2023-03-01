fd77917a782923884462330059afa2f544c2471d
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-01 15:47:56 +0000

    Collections API, will be refactored with DB values

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -1,12 +1,39 @@
+require 'json'
 # frozen_string_literal: true
 
 class DataServicesController < ApplicationController
   def index
     @data_services = DataServices::Fetcher.call(query: params[:query], filters: params[:filters])
-    @organisations_checkbox_list = Organisation.select(%i[id name]).order('name ASC')
+    @organisations = Organisation.select(%i[id name]).order('name ASC')
+
+
+    json_data = File.read('manual-organisations.json')
+    data = JSON.parse(json_data)
+    @organisations_json = data['results']
+    @min_dept = []
+    @non_min_dept = []
+    @exec_non_dept_pb = []
+    @exec_agency = []
+    @other_dept = []
+    
+    @organisations_json.each do |organisation|
+      case organisation['format']
+      when 'Ministerial department'
+        @min_dept << organisation
+      when 'Non-ministerial department'
+        @non_min_dept << organisation
+      when 'Executive non-departmental public body'
+        @exec_non_dept_pb << organisation
+      when 'Executive agency'
+        @exec_agency << organisation
+      when 'Other'
+        @other_dept << organisation
+      end
+    end
   end
 
   def show
     @data_service = DataService.includes(:organisation).find(params[:id])
   end
+
 end
