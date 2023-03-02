f693e40ba1817cf6a962273ecd607730c55f04dc
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-02 16:38:29 +0000

    Remove API json config. Hardcode dwp logo.

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -5,31 +5,6 @@ class DataServicesController < ApplicationController
   def index
     @data_services = DataServices::Fetcher.call(query: params[:query], filters: params[:filters])
     @organisations = Organisation.select(%i[id name]).order('name ASC')
-
-
-    json_data = File.read('manual-organisations.json')
-    data = JSON.parse(json_data)
-    @organisations_json = data['results']
-    @min_dept = []
-    @non_min_dept = []
-    @exec_non_dept_pb = []
-    @exec_agency = []
-    @other_dept = []
-    
-    @organisations_json.each do |organisation|
-      case organisation['format']
-      when 'Ministerial department'
-        @min_dept << organisation
-      when 'Non-ministerial department'
-        @non_min_dept << organisation
-      when 'Executive non-departmental public body'
-        @exec_non_dept_pb << organisation
-      when 'Executive agency'
-        @exec_agency << organisation
-      when 'Other'
-        @other_dept << organisation
-      end
-    end
   end
 
   def show
@@ -37,3 +12,7 @@ class DataServicesController < ApplicationController
   end
 
 end
+
+
+
+
