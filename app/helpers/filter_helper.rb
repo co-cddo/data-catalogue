94fe76dcf91f9f1cec9df80a5c0d43015597f17b
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-13 15:17:08 +0000

    make rubocop happy

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -4,15 +4,25 @@ module FilterHelper
   def render_filter_tags(filters, organisations)
     filters = Array.wrap(filters)
     content_tag :ul, class: 'moj-filter-tags' do
-      filters.map do |filter|
-        organisation = organisations.find(filter)
-        content_tag :li do
-          link_to data_services_path(query: params[:query], filters: filters.reject { |f| f == filter }), class: 'moj-filter__tag' do
-            concat content_tag(:span, organisation.name, class: 'govuk-visually-hidden')
-            concat organisation.name
-          end
-        end
-      end.join.html_safe
+      safe_join(render_filter_tag_items(filters, organisations))
     end
+  end
+
+  private
+
+  def render_filter_tag_items(filters, organisations)
+    filters.map do |filter|
+      organisation = organisations.find(filter)
+      content_tag :li do
+        link_to filter_tag_path(filter, filters), class: 'moj-filter__tag' do
+          concat content_tag(:span, sanitize(organisation.name), class: 'govuk-visually-hidden')
+          concat sanitize(organisation.name)
+        end
+      end
+    end
+  end
+
+  def filter_tag_path(filter, filters)
+    data_services_path(query: params[:query], filters: filters.reject { |f| f == filter })
   end
 end
