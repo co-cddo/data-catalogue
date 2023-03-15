63c65c1e34d6dad0a7d13def9d448aac368fdc49
Nicholas Wieland <ngw@nofeed.org>
2023-03-15 10:09:29 +0000

    Alt titles and themes have the right type

--- db/migrate/20230308160615_create_data_resources.rb
+++ db/migrate/20230308160615_create_data_resources.rb
@@ -3,7 +3,7 @@
 class CreateDataResources < ActiveRecord::Migration[7.0]
   def change
     create_table :data_resources, id: :uuid do |t|
-      t.text :alternative_title
+      t.text :alternative_titles, array: true, default: []
       t.text :contact_name
       t.text :contact_email
       t.text :description
@@ -11,7 +11,7 @@ class CreateDataResources < ActiveRecord::Migration[7.0]
       t.text :keywords, array: true, default: []
       t.text :license
       t.text :summary
-      t.text :theme, array: true, default: []
+      t.text :themes, array: true, default: []
       t.text :title
       t.text :version
       t.integer :access_rights
