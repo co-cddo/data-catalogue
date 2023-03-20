26dc251a7b5ccae9e4fe5e657b7b6b16257fe086
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-03-20 13:40:58 +0000

    Further test coverage for navigation links

--- spec/features/pagination_spec.rb
+++ spec/features/pagination_spec.rb
@@ -23,7 +23,14 @@ RSpec.describe 'Pagination' do
 
     it 'displays the pagination 3 buttons, page 1, 2 and next' do
       expect(page).to have_selector('a.govuk-link.govuk-pagination__link', count: 3)
+      expect(page).to have_content 'Next'
     end
+
+    it 'displays the pagination 3 buttons on page 2, page 1, 2 and previous' do
+        click_link '2'
+        expect(page).to have_selector('a.govuk-link.govuk-pagination__link', count: 3)
+        expect(page).to have_content 'Previous'
+      end
 
     it 'displays the next pagination navigation button' do
       expect(page).to have_selector('div.govuk-pagination__next', count: 1)
@@ -32,6 +39,29 @@ RSpec.describe 'Pagination' do
     it 'does not display the previous pagination button' do
       expect(page).not_to have_selector('div.govuk-pagination__prev')
     end
+
+    it 'shows correct results at the top of the page' do
+        expect(page).to have_content '22 results'
+    end 
+    
+    it 'shows correct results at the bottom of the page' do
+        expect(page).to have_content '1 to 20 of 22 results'
+    end 
+
+    it 'clicks next link' do
+        click_link 'Next'
+        expect(page).to have_content 'Showing 21 to 22'
+        click_link 'Previous'
+        expect(page).to have_content 'Showing 1 to 20'
+      end
+    
+      it 'navigates using number links' do
+        click_link '2'
+        expect(page).to have_content 'Showing 21 to 22'
+        click_link '1'
+        expect(page).to have_content 'Showing 1 to 20'
+      end
+    
   end
 
   context 'when results are under 20 there should be no pagination' do
@@ -48,20 +78,24 @@ RSpec.describe 'Pagination' do
       expect(page).not_to have_selector('nav.govuk-pagination')
     end
 
-    it 'does not display the pagination buttons' do
+    it 'does not display the pagination links' do
       expect(page).not_to have_selector('ul.govuk-pagination__list')
     end
 
-    it 'does not display pagination navigation buttons' do
+    it 'does not display pagination navigation links' do
       expect(page).not_to have_selector('a.govuk-link.govuk-pagination__link')
     end
 
-    it 'does not display the next pagination button' do
+    it 'does not display the next pagination link' do
       expect(page).not_to have_selector('div.govuk-pagination__next')
     end
 
-    it 'does not display the previous pagination button' do
+    it 'does not display the previous pagination link' do
       expect(page).not_to have_selector('div.govuk-pagination__prev')
     end
+
+    it 'does not display numbered pagination links' do
+        expect(page).not_to have_selector('div.govuk-pagination__link')
+      end
   end
 end
