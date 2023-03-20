83e2d5aa1317a3c43c5de40ec106570bd6ee3843
Harley Burrow <121872150+HarleyBurrowTPX@users.noreply.github.com>
2023-03-20 09:55:22 +0000

    Readding removed line

--- spec/features/pagination_spec.rb
+++ spec/features/pagination_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe 'Pagination' do
   context 'when using the pagination nav' do
     let(:organisation_test) { create(:organisation, name: 'Test Organisation') }
