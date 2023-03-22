eac6f2605adc8f3a9e5b89f08ebcff428e581506
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2023-03-22 16:05:52 +0000

    Only return the data_service id on create

--- app/controllers/api/v1/data_services_controller.rb
+++ app/controllers/api/v1/data_services_controller.rb
@@ -9,7 +9,7 @@ module Api
       def create
         data_service_form = DataServiceForm.new(data_service_params)
         if data_service_form.submit
-          render json: { data_service: data_service_form.data_service }, status: :created
+          render json: { data_service: { id: data_service_form.data_service.id } }, status: :created
         else
           render json: { errors: data_service_form.errors }, status: :unprocessable_entity
         end
