e7123e0951d14dc0fc4aceec90979d68c4ce300c
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-23 16:32:18 +0000

    Refactor, fix tests + rubocop

--- app/helpers/data_services_helper.rb
+++ app/helpers/data_services_helper.rb
@@ -1,29 +1,30 @@
 # frozen_string_literal: true
 
 module DataServicesHelper
-  def data_service_field(field_value)
-    if field_value.present?
-      if field_value.is_a?(Array)
-        field_value.map do |value|
-          if is_url(value)
-            link_to(value, value, class: 'govuk-link')
-          else
-            value
-          end
-        end.join.html_safe
-      elsif is_url(field_value)
-        link_to(field_value, field_value, class: 'govuk-link')
-      else
-        field_value
-      end
-    else
-      'n/a'
-    end
+  def render_data_service_field(value)
+    value.present? ? sanitize(format_value(value)) : 'n/a'
   end
 
   private
 
-  def is_url(value)
+  def format_value(value)
+    case value
+    when Array
+      value.map { |v| format_single_value(v) }.join(', ')
+    else
+      format_single_value(value)
+    end
+  end
+
+  def format_single_value(value)
+    url?(value) ? link_to_url(value) : value
+  end
+
+  def link_to_url(url)
+    link_to(url, url, class: 'govuk-link')
+  end
+
+  def url?(value)
     value =~ URI::DEFAULT_PARSER.make_regexp ? true : false
   end
 end
