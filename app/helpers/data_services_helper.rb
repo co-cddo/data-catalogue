df2fc61f76c80d836571f8497e0b445930bcf8cb
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-23 16:04:23 +0000

    Rubocop

--- app/helpers/data_services_helper.rb
+++ app/helpers/data_services_helper.rb
@@ -1,31 +1,29 @@
-module DataServicesHelper
+# frozen_string_literal: true
 
+module DataServicesHelper
   def data_service_field(field_value)
     if field_value.present?
       if field_value.is_a?(Array)
         field_value.map do |value|
           if is_url(value)
-            link_to(value, value, class: "govuk-link")
+            link_to(value, value, class: 'govuk-link')
           else
             value
           end
         end.join.html_safe
+      elsif is_url(field_value)
+        link_to(field_value, field_value, class: 'govuk-link')
       else
-        if is_url(field_value)
-          link_to(field_value, field_value, class: "govuk-link")
-        else
-          field_value
-        end
+        field_value
       end
     else
-      "n/a"
+      'n/a'
     end
   end
 
   private
+
   def is_url(value)
-    value =~ URI::regexp ? true : false
+    value =~ URI::DEFAULT_PARSER.make_regexp ? true : false
   end
-
 end
-
--- app/models/data_service.rb
+++ app/models/data_service.rb
@@ -5,7 +5,7 @@ class DataService < ApplicationRecord
   belongs_to :source, optional: true
   has_one :data_resource, as: :resourceable, dependent: :destroy
   has_many :creators, through: :data_resource
-  
+
   enum :service_type, { EVENT: 0, REST: 1, SOAP: 2 }
   enum :service_status, { DISCOVERY: 0, ALPHA: 1, BETA: 2, PRIVATE_BETA: 3,
                           PUBLIC_BETA: 4, LIVE: 5, DEPRECATED: 6, WITHDRAWN: 7 }
