d882423ba7b05237b2052fbbcacc2e408bb02458
lizzie cullen davison <lizzie.cullendavison@gmail.com>
2023-03-24 11:17:54 +0000

    Update enums to latest spec

--- app/models/data_service.rb
+++ app/models/data_service.rb
@@ -8,16 +8,6 @@ class DataService < ApplicationRecord
   has_one :publisher, through: :data_resource
 
   enum :service_type, { EVENT: 0, REST: 1, SOAP: 2 }
-  enum :service_status, { DISCOVERY: 0, ALPHA: 1, BETA: 2, PRIVATE_BETA: 3,
-                          PUBLIC_BETA: 4, LIVE: 5, DEPRECATED: 6, WITHDRAWN: 7 }
-
-  include PgSearch::Model
-  pg_search_scope :search,
-                  against: { name: 'A', description: 'B' },
-                  associated_against: { organisation: { name: 'C' } },
-                  using: { tsearch: { prefix: true, dictionary: 'english' } }
-
-  def name
-    read_attribute(:name) || data_resource&.title
-  end
+  enum :status, { DISCOVERY: 0, ALPHA: 1, BETA: 2, PRIVATE_BETA: 3,
+                  PUBLIC_BETA: 4, LIVE: 5, DEPRECATED: 6, WITHDRAWN: 7 }
 end
