dd4bf634f60b4cf431f48ed54c50435436a30717
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-28 18:11:24 +0100

    Reinstate parameterize

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -6,8 +6,8 @@ module OrganisationsHelper
 
     if organisation.name == 'Department for Work and Pensions'
       render partial: 'shared/dwp_logo', locals: { organisation: }
-    elsif File.exist?("app/assets/images/#{organisation.name}.png")
-      image_tag("#{organisation.name}.png", class: 'govuk-image', alt: organisation.name)
+    elsif File.exist?("app/assets/images/#{organisation.name.parameterize(separator: '')}.png")
+      image_tag("#{organisation.name.parameterize(separator: '')}.png", class: 'govuk-image', alt: organisation.name)
     else
       content_tag(:span, organisation.name)
     end
