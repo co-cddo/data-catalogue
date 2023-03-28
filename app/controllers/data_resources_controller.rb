26cdcde0e29672f800de67ee686fbf32724e489b
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-28 15:21:38 +0100

    Refactor filter/home to use publisher (name/slug)

--- app/controllers/data_resources_controller.rb
+++ app/controllers/data_resources_controller.rb
@@ -3,6 +3,6 @@
 class DataResourcesController < ApplicationController
   def index
     @pagy, @data_resources = pagy(DataResources::Fetcher.call(query: params[:query], filters: params[:filters]))
-    @organisations = Organisation.where.not(name: nil).id_name_slug_order_asc
+    @organisations = Organisation.joins(:published_resources).distinct.id_name_slug_order_asc
   end
 end
--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -2,14 +2,38 @@
 
 module OrganisationsHelper
   def organisation_logo(organisation)
-    return unless organisation && organisation.name.present?
+    return unless organisation && (organisation.name.present? || organisation.slug.present?)
 
     if organisation.name == 'Department for Work and Pensions'
-      render partial: 'shared/dwp_logo', locals: { organisation: }
-    elsif File.exist?("app/assets/images/#{organisation.name.parameterize(separator: '')}.png")
-      image_tag("#{organisation.name.parameterize(separator: '')}.png", class: 'govuk-image', alt: organisation.name)
+      render_dwp_logo(organisation)
     else
-      content_tag(:span, organisation.name)
+      render_organisation_logo(organisation)
     end
+  end
+
+  private
+
+  def render_dwp_logo(organisation)
+    render partial: 'shared/dwp_logo', locals: { organisation: }
+  end
+
+  def render_organisation_logo(organisation)
+    if organisation_logo_exists?(organisation)
+      render_organisation_logo_image(organisation)
+    else
+      render_organisation_logo_text(organisation)
+    end
+  end
+
+  def organisation_logo_exists?(organisation)
+    File.exist?("app/assets/images/#{organisation.slug}.png")
+  end
+
+  def render_organisation_logo_image(organisation)
+    image_tag("#{organisation.slug}.png", class: 'govuk-image', alt: organisation.name.presence || organisation.slug)
+  end
+
+  def render_organisation_logo_text(organisation)
+    content_tag(:span, organisation.name.presence || organisation.slug, class: 'organisation-logo-text')
   end
 end
