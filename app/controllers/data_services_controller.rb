af68900a1464fcb331bde886737379067ccc2307
Maddie Williams <maddie.williams@tpximpact.com>
2023-03-29 17:08:39 +0100

    Adjust controller action

--- app/controllers/data_services_controller.rb
+++ app/controllers/data_services_controller.rb
@@ -2,7 +2,7 @@
 
 class DataServicesController < ApplicationController
   def show
-    @data_service = DataService.includes(:creators, :publisher, data_resource: :related_resources).find(params[:id])
+    @data_service = DataService.includes(:creators, :publisher, :related_resources).find(params[:id])
     @data_resource = @data_service.data_resource
   end
 end
