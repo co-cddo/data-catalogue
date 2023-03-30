ca2edd3f61436b428a9ebbce2fed5cc3b2f7b6be
Nicholas Wieland <ngw@nofeed.org>
2023-03-30 12:05:22 +0100

    Remove unused fields

--- spec/models/data_service_spec.rb
+++ spec/models/data_service_spec.rb
@@ -4,7 +4,9 @@ require 'rails_helper'
 
 RSpec.describe DataService do
   describe 'associations' do
-    it { is_expected.to belong_to(:organisation).optional }
     it { is_expected.to have_one(:data_resource) }
+    it { is_expected.to have_one(:publisher) }
+    it { is_expected.to have_many(:creators) }
+    it { is_expected.to have_many(:related_resources) }
   end
 end
